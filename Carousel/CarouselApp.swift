a3e90b4a1184803ea9c23f86d026c992ba68280a
Azharuddin <iamazharuddin10@gmail.com>
2025-05-17 17:36:43 +0530

    Initial Commit

---
+++ Carousel/CarouselApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarouselApp.swift
+//  Carousel
+//
+//  Created by Azharuddin 1 on 17/05/25.
+//
+
+import SwiftUI
+
+@main
+struct CarouselApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CarouselTests/CarouselTests.swift
@@ -0,0 +1,17 @@
+//
+//  CarouselTests.swift
+//  CarouselTests
+//
+//  Created by Azharuddin 1 on 17/05/25.
+//
+
+import Testing
+@testable import Carousel
+
+struct CarouselTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
